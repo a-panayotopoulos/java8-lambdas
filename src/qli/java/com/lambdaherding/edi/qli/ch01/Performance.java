dc750a83e9921dfce5b142b3048e0d5e49ae5a63
Qian Li <qian.li@mastercard.com>
2017-05-14 20:54:55 +0100

    ch03 exercises and Album objects

---
+++ src/qli/java/com/lambdaherding/edi/qli/ch01/Performance.java
@@ -0,0 +1,19 @@
+package com.lambdaherding.edi.qli.ch01;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Stream.concat;
+
+public interface Performance {
+    public String getName();
+
+    public Stream<Artist> getMusicians();
+
+    // TODO: test
+    public default Stream<Artist> getAllMusicians() {
+        return getMusicians().flatMap(artist -> {
+            return concat(Stream.of(artist), artist.getMembers());
+        });
+    }
+
+}
