547531022d158873271f0155f7586e91d996b983
Robert Main <ramain75@users.noreply.github.com>
2017-05-15 23:08:22 +0100

    Questions 1a, b, c and started typing in question 2 but ran out of time

---
+++ src/ram/java/com/lamdaherding/edi/ram/ch03/Ex1.java
@@ -0,0 +1,23 @@
+package com.lamdaherding.edi.ram.ch03;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+
+public class Ex1 {
+
+	public int addUp(Stream<Integer> numbers) {
+		return numbers.reduce( 0, (acc, element) -> acc + element);
+	}
+
+	public List<String> getNamesAndOrigin(Stream<Artist> artists) {
+		return artists.map(artist -> (artist.getName() + ", " + artist.getPlaceOfOrigin()))
+				      .collect(toList());
+	}
+	
+	public List<Album> getAlbumsWithAtMost3Tracks(Stream<Album> albums) {
+		return albums.filter(album -> album.getTracks().size() <= 3)
+				.collect(toList());
+	}
+}
