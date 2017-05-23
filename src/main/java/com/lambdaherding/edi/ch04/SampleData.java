271c6881632521ebade5505ff09a3c2802ab2388
axp <alex_panayotopoulos@mastercard.com>
2017-05-23 11:24:41 +0100

    Let's make this an interface then

--- src/main/java/com/lambdaherding/edi/ch04/SampleData.java
+++ src/main/java/com/lambdaherding/edi/ch04/SampleData.java
@@ -6,28 +6,28 @@ import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 
-public class SampleData {
-	public static final Artist JOHN_COLTRANE = new Artist("John Coltrane", "US");
-	public static final Artist JOHN_LENNON = new Artist("John Lennon", "UK");
-	public static final Artist PAUL_MCCARTNEY = new Artist("Paul McCartney", "UK");
-	public static final Artist GEORGE_HARRISON = new Artist("George Harrison", "UK");
-	public static final Artist RINGO_STARR = new Artist("Ringo Starr", "UK");
+public interface SampleData {
+	public Artist JOHN_COLTRANE = new Artist("John Coltrane", "US");
+	public Artist JOHN_LENNON = new Artist("John Lennon", "UK");
+	public Artist PAUL_MCCARTNEY = new Artist("Paul McCartney", "UK");
+	public Artist GEORGE_HARRISON = new Artist("George Harrison", "UK");
+	public Artist RINGO_STARR = new Artist("Ringo Starr", "UK");
 
-	public static final List<Artist> MEMBERS_OF_THE_BEATLES =
+	public List<Artist> MEMBERS_OF_THE_BEATLES =
 			Arrays.asList( JOHN_LENNON, PAUL_MCCARTNEY, GEORGE_HARRISON, RINGO_STARR );
 
-	public static final Artist THE_BEATLES = new Artist("The Beatles", MEMBERS_OF_THE_BEATLES, "UK");
+	public Artist THE_BEATLES = new Artist("The Beatles", MEMBERS_OF_THE_BEATLES, "UK");
 
-	public static final Album A_LOVE_SUPREME =
+	public Album A_LOVE_SUPREME =
 			new Album("A Love Supreme", asList(
 					new Track("Acknowledgement", 467),
 					new Track("Resolution", 442)),
 					asList(JOHN_COLTRANE));
 
-	public static final Album SAMPLE_SHORT_ALBUM =
+	public Album SAMPLE_SHORT_ALBUM =
 			new Album("sample Short Album", asList(new Track("short track", 30)), asList(JOHN_COLTRANE));
 
-	public static final Album MANY_TRACK_ALBUM =
+	public Album MANY_TRACK_ALBUM =
 			new Album("sample Short Album", asList(
 					new Track("short track", 30),
 					new Track("short track 2", 30),
