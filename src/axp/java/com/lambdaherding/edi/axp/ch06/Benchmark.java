cd60af80943479df10743e588f97ba59a898fe26
axp <alex_panayotopoulos@mastercard.com>
2017-06-09 17:08:22 +0100

    Finish exercises

--- src/axp/java/com/lambdaherding/edi/axp/ch06/Benchmark.java
+++ src/axp/java/com/lambdaherding/edi/axp/ch06/Benchmark.java
@@ -3,7 +3,14 @@ package com.lambdaherding.edi.axp.ch06;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * Benchmark some runnable method.
+ */
 public class Benchmark {
+	/**
+	 * Benchmark a method which requires no input
+	 * @param runnable the method to test
+	 */
 	public static void benchmark( Runnable runnable ) {
 		long time = System.nanoTime();
 		runnable.run();
@@ -11,14 +18,29 @@ public class Benchmark {
 		System.out.printf( "%,d\n", time );
 	}
 
+	/**
+	 * Benchmark a consumer against some input
+	 * @param consumer the method to test
+	 * @param input input to the method
+	 */
 	public static <T> void benchmark( Consumer<T> consumer, T input ) {
 		benchmark( () -> consumer.accept( input ) );
 	}
 
+	/**
+	 * Benchmark a function against some input
+	 * @param function the method to test
+	 * @param input input to the method
+	 */
 	public static <T> void benchmark( Function<T,?> function, T input ) {
 		benchmark( () -> function.apply( input ) );
 	}
 
+	/**
+	 * Benchmark a consumer against a range of input
+	 * @param consumer the method to test
+	 * @param input an array of possible inputs
+	 */
 	@SafeVarargs
 	public static <T> void benchmark( Consumer<T> consumer, T...input ) {
 		for ( int i = 0; i < input.length; i++ ) {
@@ -28,6 +50,11 @@ public class Benchmark {
 		}
 	}
 
+	/**
+	 * Benchmark a function against some input
+	 * @param function the method to test
+	 * @param input an array of possible inputs
+	 */
 	@SafeVarargs
 	public static <T> void benchmark( Function<T,?> function, T...input ) {
 		for ( int i = 0; i < input.length; i++ ) {
--- src/axp/java/com/lambdaherding/edi/axp/ch06/SumOfSquaresRedux.java
+++ src/axp/java/com/lambdaherding/edi/axp/ch06/SumOfSquaresRedux.java
@@ -12,82 +12,78 @@ import java.util.stream.IntStream;
 
 public class SumOfSquaresRedux {
 	/**
-	 * The slow form of the function
+	 * The slow form of the function, using boxed integers
 	 * @param range
 	 * @return
 	 */
-	public static int slowSumOfSquares( List<Integer> linkedListOfNumbers ) {
+	public static int boxedSumOfSquares( List<Integer> linkedListOfNumbers ) {
 		return linkedListOfNumbers.parallelStream()
 				.map(x -> x * x)
 				.reduce(0, (acc, x) -> acc + x);
 	}
 
 	/**
-	 * The faster form of the function
-	 * 
-	 * - Use mapToInt instead of map
-	 * 
+	 * The faster form of the function, unboxed using mapToInt
 	 * @param range
 	 * @return
 	 */
-	public static int fasterSumOfSquares( List<Integer> linkedListOfNumbers ) {
+	public static int unboxedSumOfSquares( List<Integer> linkedListOfNumbers ) {
 		return linkedListOfNumbers.parallelStream()
 				.mapToInt( x -> x * x )
 				.reduce(0, (acc, x) -> acc + x);
 	}
 
 	/**
-	 * The fastest form of the function
-	 * 
-	 * - Use sum instead of reduce
-	 * 
+	 * The fastest form of the function, using sum instead of reduce
 	 * @param range
 	 * @return
 	 */
-	public static int fastestSumOfSquares( List<Integer> linkedListOfNumbers ) {
+	public static int noReduceSumOfSquares( List<Integer> linkedListOfNumbers ) {
 		return linkedListOfNumbers.parallelStream()
 				.mapToInt( x -> x * x )
 				.sum();
 	}
 
 	/**
-	 * Benchmark both forms
+	 * Benchmark all versions
 	 * 
 	 * Result:
-	 * | input size | Sequential | Parallel  |
-	 * +------------+------------+-----------+
-	 * |      1,000 |     90,902 |   108,496 |
-	 * |     10,000 |    498,497 |    74,042 |
-	 * |    100,000 |  1,958,432 |    79,173 |
-	 * |  1,000,000 |  3,817,898 |   256,579 |
-	 * | 10,000,000 | 11,032,170 | 2,097,351 |
+	 * | input size  | boxed         | unboxed    | using sum  |
+	 * +-------------+---------------+------------+------------+
+	 * |       1,000 |       248,515 |    212,594 |     89,069 |
+	 * |      10,000 |     3,305,473 |    715,123 |     50,949 |
+	 * |     100,000 |     2,548,931 |  4,223,659 |    182,172 |
+	 * |   1,000,000 |    15,055,331 | 18,514,385 |    506,560 |
+	 * |  10,000,000 |   296,424,056 |  4,294,035 |  4,563,444 |
+	 * | 100,000,000 | 1,776,488,773 | 40,166,366 | 39,608,489 |
 	 * 
-	 * As you can see, it's not worth parallelising if you've got less than
-	 * a thousand inputs. Also, the sequential performance is a lot more
-	 * linear than the parallel performance.
+	 * Unboxed functions quickly become more efficient than boxed ones, and using "sum" instead of
+	 * "reduce" makes it even faster. Interestingly, however, this distinction starts to go away at
+	 * higher numbers. (This is not just a fluke of this test run-- it shows the same behaviour on
+	 * multiple runs.)
 	 * 
 	 * @param args
 	 */
 	public static void main( String[] args ) {
-		List<Integer>[] inputt = randommInput( 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000 );
+		List<Integer>[] input = randomInput( 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000 );
 
 		// Throw one away
-		slowSumOfSquares( inputt[0] );
-		fasterSumOfSquares( inputt[0] );
-		fastestSumOfSquares( inputt[0] );
+		boxedSumOfSquares( input[0] );
+		unboxedSumOfSquares( input[0] );
+		noReduceSumOfSquares( input[0] );
 
-		System.out.println( "Benchmarking slow form of function" );
-		Benchmark.benchmark( SumOfSquaresRedux::slowSumOfSquares, inputt );
+		System.out.println( "Benchmarking boxed form of function" );
+		Benchmark.benchmark( SumOfSquaresRedux::boxedSumOfSquares, input );
 
-		System.out.println( "Benchmarking faster form of function" );
-		Benchmark.benchmark( SumOfSquaresRedux::fasterSumOfSquares, inputt );
+		System.out.println( "Benchmarking unboxed form of function" );
+		Benchmark.benchmark( SumOfSquaresRedux::unboxedSumOfSquares, input );
 
-		System.out.println( "Benchmarking fastest form of function" );
-		Benchmark.benchmark( SumOfSquaresRedux::fastestSumOfSquares, inputt );
+		System.out.println( "Benchmarking no-reduce form of function" );
+		Benchmark.benchmark( SumOfSquaresRedux::noReduceSumOfSquares, input );
 	}
 
 	@SuppressWarnings("unchecked")
-	public static List<Integer>[] randommInput( int limit, int...sizes ) {
+	public static List<Integer>[] randomInput( int limit, int...sizes ) {
 		Random rand = new Random();
 
 		return Arrays.stream( sizes )
