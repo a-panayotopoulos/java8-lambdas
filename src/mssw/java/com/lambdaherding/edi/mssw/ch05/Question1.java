7050f00318a138b70acd531d9d01c55393b47048
Michael Wambeek <michael_wambeek@mastercard.com>
2017-06-13 13:42:57 +0100

    adding question 1

---
+++ src/mssw/java/com/lambdaherding/edi/mssw/ch05/Question1.java
@@ -0,0 +1,22 @@
+package com.lambdaherding.edi.mssw.ch05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by E047027 on 13/06/2017.
+ */
+public class Question1 {
+
+    public void mapToUpperCase(){
+        List<String> collected = Stream.of("a", "b", "hello")
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        assertEquals(Arrays.asList("A", "B", "HELLO"), collected);
+    }
+}
