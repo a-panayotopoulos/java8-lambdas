2b2a26dcc8afc58b28beb06de426905fc7c1e678
Michael Wambeek <michael_wambeek@mastercard.com>
2017-06-20 09:45:56 +0100

    finishing question 2

--- src/mssw/java/com/lambdaherding/edi/mssw/ch05/Question2.java
+++ src/mssw/java/com/lambdaherding/edi/mssw/ch05/Question2.java
@@ -1,5 +1,6 @@
 package com.lambdaherding.edi.mssw.ch05;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -11,6 +12,13 @@ public final class Question2 {
         Stream<String> names = Stream.of("John Lennon", "Paul McCartney",
                 "George Harrison", "Ringo Starr", "Pete Best", "Stuart Sutcliffe");
 
+        String longestName = names.collect(Collectors.reducing("", (acc, element) -> {
+            if (element.length() > acc.length()) {
+                return element;
+            }
+            return acc;
+        }));
 
+        System.out.println("Longest name: " + longestName);
     }
 }
