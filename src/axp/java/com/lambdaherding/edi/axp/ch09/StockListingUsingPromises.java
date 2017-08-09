2292fd9b965c0d5eaff635787470972573289f1c
axp <alex_panayotopoulos@mastercard.com>
2017-08-09 17:08:15 +0100

    Use CompletableFuture#thenApply to simplify

--- src/axp/java/com/lambdaherding/edi/axp/ch09/StockListingUsingPromises.java
+++ src/axp/java/com/lambdaherding/edi/axp/ch09/StockListingUsingPromises.java
@@ -9,7 +9,6 @@ import java.util.Currency;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,41 +21,30 @@ import com.lambdaherding.edi.axp.ch09.service.StockService;
 /**
  * Migrate {@link StockListingUsingFutures} to {@link CompletableFuture}s.
  * 
- * We haven't really touched on the power of completable futures yet.
+ * Using {@link CompletableFuture#thenApply} means that we only have to collect
+ * to an intermediate result once, which drastically simplifies the code.
  */
 public class StockListingUsingPromises {
 	static StockService valuer = new DummyStockService( 1000L );
 	static ConversionService converter = new DummyConversionService( 500L );
 	static ExecutorService executor = Executors.newFixedThreadPool( 3 );
 
-	private static <E> CompletableFuture<E> getPromise( Supplier<E> supplier ) {
-		return CompletableFuture.supplyAsync( supplier, executor );
-	}
-
 	public static void main( String...args ) {
 		STOCKS.stream()
-			.map( s -> getPromise( () -> getStockValue( s ) ) )
-			.collect( Collectors.toList() )
-			.stream()
-			.map( CompletableFuture::join )
-			.map( s -> getPromise( () -> getDailyChange( s ) ) )
+			.map( s -> CompletableFuture.supplyAsync( () -> getStockValue( s ), executor ) )
+			.map( f -> f.thenApply( StockListingUsingPromises::getDailyChange ) )
+			.flatMap( s -> valueInCurrencies( s, USD, GBP, EUR ) )
 			.collect( Collectors.toList() )
 			.forEach( CompletableFuture::join );
 
-		STOCKS.forEach( s -> valueInCurrencies( s, USD, GBP, EUR ) );
 		STOCKS.forEach( System.out::println );
 
 		executor.shutdownNow();
 	}
 
-	public static void valueInCurrencies( Stock stock, Currency...currencies ) {
-		Currency base = stock.nativeCurrency();
-
-		Stream.of( currencies )
-			.filter( c -> !c.equals( base ) )
-			.map( c -> getPromise( () -> fillCurrency( stock, c ) ) )
-			.collect( Collectors.toList() )
-			.forEach( CompletableFuture::join );
+	public static Stream<CompletableFuture<Stock>> valueInCurrencies( CompletableFuture<Stock> stock, Currency...currencies ) {
+		return Stream.of( currencies )
+			.map( c -> stock.thenApply( s -> fillCurrency( s, c ) ) );
 	}
 
 	public static Stock getStockValue( Stock stock ) {
@@ -68,6 +56,7 @@ public class StockListingUsingPromises {
 	}
 
 	public static Stock fillCurrency( Stock stock, Currency curr ) {
-		return stock.value( curr, converter.getRelativeValue( stock.nativeCurrency(), curr ) );
+		return curr.equals( stock.nativeCurrency() ) ? stock :
+			stock.value( curr, converter.getRelativeValue( stock.nativeCurrency(), curr ) );
 	}
 }
